ea382f6b76b744269e9fe27ae943a90888f4656e
SolarisSchema <tonyrouxrito50@gmail.com>
2020-10-07 01:48:47 -0500

    basic animation script

--- manager.js
+++ manager.js
@@ -1,1 +1,30 @@
-    
+var t = 0;
+
+function EarthGo() {
+    t += 0.005;
+
+    var r = 100;
+    var xcenter = 100;
+    var ycenter = 100;
+    var newLeft = Math.floor(xcenter + (r * Math.cos(t)));
+    var newTop = Math.floor(ycenter + (r * Math.sin(t)));
+    $('#flatEarth').animate({
+        top: newTop,
+        left: newLeft,
+    }, 1, function() {
+        moveit();
+    });
+}
+
+
+
+
+
+
+
+
+
+//call function once page is ready
+$(document).ready(function() {
+    EarthGo();
+});
