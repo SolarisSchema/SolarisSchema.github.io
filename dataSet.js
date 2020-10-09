445df4e45db31e0a44f73787d6f1fd089b0822af
SolarisSchema <tonyrouxrito50@gmail.com>
2020-10-09 00:44:39 -0500

    added mercury data

--- dataSet.js
+++ dataSet.js
@@ -3,12 +3,25 @@
 
 var dataSet = {
     sun: {
-        moons: "N/A",
-        diameter: "1392678.02 km",
-        mass: "1.9891 × 10^30 kg",
-        orbit: "N/A",
-        surfaceArea: "6.09 × 10^12 km^2",
-        distance: "N/A",
-        img: "flatSun.png",
+        sun: {
+            moons: "N/A",
+            diameter: "1392678.02 km",
+            mass: "1.9891 × 10^30 kg",
+            orbit: "N/A",
+            surfaceArea: "6.09 × 10^12 km^2",
+            distance: "N/A",
+            img: "flatSun.png",
+        }
     },
+    planets: {
+        mercury: {
+            moons: "0",
+            diameter: "4880 km",
+            mass: "3.3011 × 10^23 kg",
+            orbit: "87.9691 DAYS",
+            surfaceArea: "7.48 × 10^7 km^2",
+            distance: "~0.387098 AU",
+            img: "images/planets/flatMercury.png",
+        }
+    }
 }
