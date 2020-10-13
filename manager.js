50a1d3d5aca87c7e94009e3da520838765ea4a2c
SolarisSchema <72467577+SolarisSchema@users.noreply.github.com>
2020-10-13 13:39:45 -0500

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,6 +1,7 @@
 var panelT = 0;
 var shown = false;
 var load = false;
+var loadFact = false
 var Met = 0;
     var Mer = 75;
 var Vt = 0;
@@ -265,6 +266,7 @@ function statPage(body, orbtBody) {
     var Hrings = body.Hrings;
     var distFrom = orbtBody;
     var hstSpltChk = "";
+    var funFact = body.FunFact;
     //debug check
     console.log(body + ", " + orbtBody);
     //console.log(Moons + ", " + Dia + ", " + Mass + ", " + Orbit + ", " + SArea + ", " + Dist);
@@ -287,6 +289,8 @@ function statPage(body, orbtBody) {
         $('<p class="statPage" id="labelText4"><b>ORBITAL PERIOD:</b></p>').appendTo("#statsDiv").fadeIn(1000);
         $('<p class="statPage" id="labelText5"><b>SURFACE AREA:</b></p>').appendTo("#statsDiv").fadeIn(1000);
         $('.btn3').fadeIn(1000);
+        $('<div class="btn4" id="factSub" onclick="addFact(' + funFact + ')"></div>').appendTo('body');
+        $('<p class="btn4 btnTxt" id="factText"><b>&gt GENERATE A FACT &lt</b></p>').appendTo('#factSub');
         if (distFrom.indexOf(' ') >= 0) {
             console.log("true")
             hstSpltChk = orbtBody.split(" ");
@@ -297,6 +301,20 @@ function statPage(body, orbtBody) {
         $('<a class="statPage" href="https://solarisschema.github.io" id="returnLink">&gt RETURN TO OVERVIEW &lt</a>').appendTo("#statsDiv").fadeIn(1000);
         $('<div class="statPage" id="textShift"></div>').prependTo("#statsDiv").fadeIn(1000);
     }, 1500);
+}
+
+function addFact(list) {
+    console.log(list)
+    if (shown == true) {
+        if (loadFact == true) {
+            $('#fact').remove()
+            loadFact = false;
+        } 
+        var currFact = list[Math.floor(Math.random() * list.length)];
+        $('<p class="statPage" id="fact" style="color: rgb(237, 229, 187); position: absolute; font-family: 'Dosis', sans-serif; left:10%; top:40%; padding:2%;"><b>' + currFact + '</b></p>').appendTo('#statsDiv').fadeIn(1000);
+    } else {
+        return;
+    }
 }
 
 function panelGen(topic, host) {
